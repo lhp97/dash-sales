3647152a1a06968fd9e536826e48cf8fea8d3732
lhp97 <lhdprado@gmail.com>
2022-09-13 20:38:42 -0300

    Create projects monorepo

---
+++ backend/src/main/java/com/projeto/dashsales/DashsalesApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.dashsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashsalesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projeto/dashsales/DashsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.dashsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
