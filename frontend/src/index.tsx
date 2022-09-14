6f112640054eb0dca4388ad05601d27acd632fa9
lhp97 <lhdprado@gmail.com>
2022-09-14 06:35:13 -0300

    Bootstrap include

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Assets/css/style.css';
 import App from './App';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
